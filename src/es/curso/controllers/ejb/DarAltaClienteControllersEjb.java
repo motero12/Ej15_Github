2cfbc317cda5c0d421fd36349f1dc47637c61cd6
Usuario <Usuario@192.168.1.34>
2015-05-22 11:57:11 +0200

    ADD ClienteControllers method=Agregar

---
+++ src/es/curso/controllers/ejb/DarAltaClienteControllersEjb.java
@@ -0,0 +1,19 @@
+package es.curso.controllers.ejb;
+
+import es.curso.controllers.DarAltaClienteControllers;
+import es.curso.model.entity.Cliente;
+
+public class DarAltaClienteControllersEjb implements DarAltaClienteControllers{
+
+	@Override
+	public void agregar(Cliente cliente) {
+		// TODO Auto-generated method stub
+		// poner la logica del negocio para agregar un cliente
+		// 1. verificar datos
+		// 2. agregarlos ----> llamar a la capa dao ----> para dar de alta
+		// 3. enviar e-mail al jefe
+		// 4. enviar e-mail al cliente
+		 
+	}
+	
+}
